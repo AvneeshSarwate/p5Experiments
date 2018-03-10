43d97506c455cd3426196370e672f8f61d851efb
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-03-10 15:34:26 -0500

    basic line drawing working

--- senselDrawing.js
+++ senselDrawing.js
@@ -62,23 +62,23 @@ function sensel(){
 	
 	console.log(frameTouches, newTouches, activeTouches, deadTouches);
 	
-	// newTouches.forEach(function(touchId) { //add start point of draw curve for new touches
-	// 	touchToCurve[touchId] = curveCounter;
-	// 	curveTracker[curveCounter] = [[frameTouchData[touchId]], [random(255), random(255), random(255)]];//list of points in curve and color
-	// 	//console.log(touchId, curveCounter);
-	// 	curveCounter++;
-	// });
-	// activeTouches.difference(newTouches).forEach(function(touchId){//draw the newest curve segment for each active touch
-	// 	var curveId = touchToCurve[touchId];
-	// 	var curvePoints = curveTracker[curveId][0];
-	// 	var newPoint = frameTouchData[touchId];
-	// 	var lastPoint = curvePoints[curvePoints.length-1];
-	// 	var curveColor = curveTracker[curveId][1];
-	// 	stroke(curveColor[0], curveColor[1], curveColor[2]);
-	// 	strokeWeight(newPoint[2]/senselF * 10);
-	// 	line(lastPoint[0], lastPoint[1], newPoint[0], newPoint[1]);
-	// 	curvePoints.push(newPoint);
-	// });
+	newTouches.forEach(function(touchId) { //add start point of draw curve for new touches
+		touchToCurve[touchId] = curveCounter;
+		curveTracker[curveCounter] = [[frameTouchData[touchId]], [random(255), random(255), random(255)]];//list of points in curve and color
+		//console.log(touchId, curveCounter);
+		curveCounter++;
+	});
+	activeTouches.difference(newTouches).forEach(function(touchId){//draw the newest curve segment for each active touch
+		var curveId = touchToCurve[touchId];
+		var curvePoints = curveTracker[curveId][0];
+		var newPoint = frameTouchData[touchId];
+		var lastPoint = curvePoints[curvePoints.length-1];
+		var curveColor = curveTracker[curveId][1];
+		stroke(curveColor[0], curveColor[1], curveColor[2]);
+		strokeWeight(newPoint[2]/senselF * 150);
+		line(lastPoint[0] * drawXscale, lastPoint[1] * drawYscale, newPoint[0] * drawXscale, newPoint[1] * drawYscale);
+		curvePoints.push(newPoint);
+	});
 
 	activeTouches = activeTouches.union(newTouches);
 }
